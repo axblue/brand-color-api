4fa358c4a198af19bb6e94bc9490e02d7a3dcb2b
axblue <axblue@yandex.ru>
2021-02-26 01:19:20 +0300

    Update brand.controller.js

--- src/controllers/brand.controller.js
+++ src/controllers/brand.controller.js
@@ -6,13 +6,14 @@ module.exports.getColorByBrand = async (req, res) => {
   try {
     const url = req.query.url.match(/https?:\/\/[^/]+((?=\/)|$)/g)[0];
     const brand = await brandModel.getBrandByUrl(url);
+
     if (brand.rows.length !== 0) {
       return res.status(200).json({ color1: brand.rows[0].color1 });
     } else {
       return res.status(200).json(
         await browser.addToQueue(url).then(({ colors }) => {
           brandModel.insertBrand(url, colors[0]);
-          return { color1: colors[0] };
+          return colors;
         })
       );
     }
